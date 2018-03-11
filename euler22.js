5d4d1a37e90f4df6c1d25de065574284c26008ee
Francisco Rafart <francisco.rafart@gmail.com>
2018-03-11 17:32:27 -0400

    first solution

---
+++ euler22.js
@@ -0,0 +1,68 @@
+let fs = require('fs')
+let file;
+
+let dict = {
+  a: 1, b: 2, c: 3, d: 4, e: 5, f: 6, g: 7, h: 8, i: 9, j: 10, k: 11, l: 12, m: 13, n: 14, o: 15,
+  p: 16, q:17, r: 18, s: 19, t: 20, u: 21, v: 22, w: 23, x: 24, y: 25, z: 26,
+}
+
+fs.readFile('./p022_names.txt', (err, data) => {
+  file = data.toString();
+
+  //execute the functions
+  let nameArray = extractNames(file)
+  orderAlph(nameArray);
+  let alphValsArr = alphValues(nameArray, dict);
+  let scoreArr = nameScores(alphValsArr)
+  console.log(addAll(scoreArr))
+})
+
+
+
+function extractNames(file){
+  //extract with reg exp and add to array
+  let regex = /([A-Z])\w+/g
+  let array = file.match(regex) // thing.length = 5163
+
+  return array
+}
+
+//order alphabetically
+function orderAlph(arr){
+  arr.sort() //try to implement this function by myself
+}
+function alphValues(arr, dic){
+  let values = []
+  for (name of arr){
+    totalname = 0
+    for (ch of name){
+      let num = dic[ch.toLowerCase()]
+      totalname += num
+    }
+    values.push(totalname)
+  }
+  return values
+}
+
+function nameScores(alphValArray){
+  let res = []
+  for (i in alphValArray){
+    let index = Number(i)+1
+    let namescore = alphValArray[i] * index
+
+    res.push(namescore)
+  }
+  return res
+}
+
+function addAll(arr){
+  total = 0
+  for (let num of arr){
+    total += num
+  }
+  return total
+}
+
+//name score = alphabetical value * oder in list
+
+//Sum all the name scores
